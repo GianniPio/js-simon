a2cc9ea112218a8f60a0e1866bb5175e025643a0
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-09 14:19:38 +0100

    logical steps

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 // Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// 1. Creare un array con 5 numeri random
+
+// 2. Far vedere questi numeri per 30 secondi
+    // 2.1 Far partire timer di 30 secondi
+    // 2.2 Dopo i 30 secondi, i numeri scompaiono
+
+// 3. Tramite il promt, far inserire all'utente 5 numeri
+
+// 4. In base a quanti numeri ha indovinato, esce un messaggio
